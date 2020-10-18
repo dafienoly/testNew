3271020de7d616534c8d97f964cb066e84248e74
liuyang <liuyang6790@163.com>
2020-10-18 13:31:43 +0800

    first commit

---
+++ C++/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int b = 0;
+    cin >> b;
+    cout << "hello word" << endl; 
+    getchar();
+    return 0;
+}
+
---
+++ Python/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test.py
+@Time    :   2020/10/18 13:21:28
+@Author  :   liuyang
+@Version :   1.0
+@Contact :   liuyang6890@163.com
+@License :   (C)Copyright Liuyang
+@Desc    :   None
+'''
+
+# here put the import lib
+print 'hello world'
